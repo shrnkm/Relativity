f06fdfbdfef0a42437e75fabaf580ba9bd648fa6
shrnkm <sahar.niknam@gmail.com>
2020-09-09 19:09:44 +0200

    add basics

---
+++ Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody pl;
+    void Start()
+    {
+        pl = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        //define float moveY = Input.GetAxis ("");
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        pl.position = pl.position + 0.2f  * movement;
+    }
+}
